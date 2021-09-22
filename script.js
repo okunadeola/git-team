830e1503ff2d46448e46beb1a8ed049db1017911
jassy1995 <babatundejoseph85@gmail.com>
2021-09-22 11:09:57 +0100

    i added the logic

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const targetValue=(a)=>{
-		if(scr.value==0){
-            scr.value =a;
-		}else{
-			scr.value +=a;
-		}
-	}
-const totalValue=()=>{
-		scr.value = eval(scr.value);
+function targetValue(a) {
+  if (scr.value == 0) {
+    scr.value = a;
+  } else {
+    scr.value += a;
+  }
+}
 
-	}
+function totalValue() {
+  scr.value = eval(scr.value);
+}
+
+function del1(jp) {
+  scr.value = scr.value.substring(0, scr.value.length - 1);
+  if (scr.value == "") {
+    scr.value = 0;
+  }
+}
+
+function specialFunction(v) {
+  var myVal;
+  var convertToDegree = (scr.value * Math.PI) / 180;
+  if (v == "sin") {
+    myVal = Math.sin(convertToDegree);
+  } else if (v == "cos") {
+    myVal = Math.cos(convertToDegree);
+  } else if (v == "tan") {
+    myVal = Math.tan(convertToDegree);
+  }
+  scr.value = myVal;
+}
